73a8d5d10dab9236cc68e4781f96a61ebe8a56a5
Bao Tran <btrnaoa@gmail.com>
2021-01-22 19:58:30 +1100

    Add deploy config

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
-const app = require('express')();
+const express = require('express');
+const path = require('path');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-const PORT = process.env.PORT | 3001;
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 io.on('connection', (socket) => {
   console.log('a user connected');
@@ -13,6 +17,10 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 http.listen(PORT, () => {
