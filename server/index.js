f7a5b44e97633786d40b346be16101431d93ac46
Bao Tran <btrnaoa@gmail.com>
2021-02-03 08:20:06 +1100

    Update dev mode config

--- server/index.js
+++ server/index.js
@@ -27,9 +27,11 @@ io.on('connection', (socket) => {
   });
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
+}
 
 http.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
