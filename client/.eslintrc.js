90be29dc949a676d002333278a19726975b74b2a
Bao Tran <btrnaoa@gmail.com>
2021-04-02 21:56:23 +1100

    Update config

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: ['react-app', 'airbnb-typescript', 'prettier'],
+  extends: ['react-app', 'airbnb-typescript', 'prettier', 'prettier/react'],
   rules: {
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
